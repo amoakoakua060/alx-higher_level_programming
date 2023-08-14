fab07e3a3a9472332e11b43881e02d3f6c220ce9
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-14 00:11:14 -0700

    done with task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    if matrix_len == 0 or (matrix_len == 1 and len(matrix[0]) == 0):
+        print()
+
+    if not isinstance(matrix, list):
+        return None
+
+    for i in range(matrix_len):
+        if isinstance(matrix[i], list):
+            for j in range(len(matrix[i])):
+                if j + 1 == len(matrix[i]):
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d} ".format(matrix[i][j]), end="")
