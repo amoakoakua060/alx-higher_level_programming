2847f6138bcc8c7605b48d9cc38fbc77d7ca0072
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-13 23:50:35 -0700

    done with task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        for a in range(1, len(my_list) + 1):
+            print("{:d}".format(my_list[a * -1]))
+        my_list.reverse()
