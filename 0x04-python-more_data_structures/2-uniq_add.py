f791c7f529f2f16b9f8eec1977d15dad05b01fe2
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-17 00:15:28 -0700

    made changes to task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,4 +4,4 @@ def uniq_add(my_list=[]):
     if isinstance(my_list, list) and len(my_list):
         for i in list(set(my_list)):
             num_sum += i
-        return num_sum
+    return num_sum
