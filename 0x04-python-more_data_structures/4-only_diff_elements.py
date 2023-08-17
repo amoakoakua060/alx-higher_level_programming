42815e393ca263458ee3b3a157e2ce2106148197
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-16 23:47:42 -0700

    made changes to task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     if isinstance(set_1, set) and not isinstance(set_2, set):
         return set_1
     elif isinstance(set_2, set) and not isinstance(set_1, set):
