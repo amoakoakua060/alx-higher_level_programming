4bcec5fbd5dda170536aa9c74d6e660704ebadae
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-28 19:04:17 -0700

    done with task 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" containing raise_exception_msg function"""
+
+
+def raise_exception_msg(message=""):
+    """raises a name exception with a message"""
+    raise NameError(message)
