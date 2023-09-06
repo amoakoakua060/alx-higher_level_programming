b534c468ba924ff6e393607d5d05fac635fe045a
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-05 22:58:56 -0700

    done with task 0 function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Module containing an add_integer function
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,11 @@
+# HIGH LEVEL PROGRAMMING
+
+## 0x07. Python - Test-driven development
+
+Objectives of the project are:
+- What’s an interactive test
+- Why tests are important
+- How to write Docstrings to create tests
+- How to write documentation for each module and function
+- What are the basic option flags to create tests
+- How to find edge cases
