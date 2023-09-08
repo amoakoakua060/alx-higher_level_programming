7b82b9f7be3733397f3f7760e8b0414f59b64b3f
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-07 23:43:32 -0700

    made corrections to task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -16,17 +16,17 @@ class Rectangle():
         """
         Instantiate the class and set private properties
         """
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-
-        if height < 0:
-            raise ValueError("height must be >= 0")
-
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
 
         if width < 0:
             raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
 
         self.__height = height
         self.__width = width
