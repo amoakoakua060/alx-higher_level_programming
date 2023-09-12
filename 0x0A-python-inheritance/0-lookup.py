eae56cd2dda3fb67ea12067f31710a589c637340
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-11 19:55:48 -0700

    done with task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Contains the lookup function
+"""
+
+
+def lookup(obj):
+    """
+    returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
