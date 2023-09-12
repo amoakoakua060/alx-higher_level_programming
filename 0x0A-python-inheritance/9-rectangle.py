7f71e545033f510e65fe33789714d6bd27cfeb11
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-11 21:04:17 -0700

    done with task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+module containing Rectangle class
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A Rectangle geometry class
+    """
+
+    def __init__(self, width, height):
+        """
+        instantiate rectangle class
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        returns the area of the Rectangle
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        returns string representation in the form [Rectangle] <width>/<height>
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
