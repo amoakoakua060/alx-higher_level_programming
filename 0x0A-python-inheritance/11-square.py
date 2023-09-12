c4a65a83fcf271a7b637afd2250b5c08a6484a20
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-11 21:19:21 -0700

    done with task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+module containing Square class
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square geometry class
+    """
+
+    def __init__(self, size):
+        """
+        instantiate new object private attributes
+        """
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def area(self):
+        """
+        returns the area of the Square
+        """
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """ String representation of the Class"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
