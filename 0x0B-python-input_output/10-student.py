a4db0178973714140188e51222806222ad23f0a8
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-12 21:05:42 -0700

    done with task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+module containing Student class
+"""
+
+
+class Student():
+    """ Student Class with instance variables """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of new Student object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary representation of Student class
+        """
+
+        if isinstance(attrs, list):
+            return {k: self.__dict__[k] for k in self.__dict__ if k in attrs}
+        return self.__dict__
