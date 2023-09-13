3f829f6208c394a7a56971bea1f216587444e444
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-12 20:13:26 -0700

    done with task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+module containing save_to_json_file function
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an Object to a text file, using a JSON representation
+    """
+
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
