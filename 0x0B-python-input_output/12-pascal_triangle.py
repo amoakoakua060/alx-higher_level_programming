baf657f8cac682b1f975ebb60f47c1c2a152cac5
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-12 21:26:09 -0700

    yet to implement pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+module containing pascal_triangle function
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+
+    matrix = []
+    return matrix
