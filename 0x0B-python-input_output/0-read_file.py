8c66aa126b06c9d0162af8b5e18c95fba69919aa
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-12 19:38:43 -0700

    done with task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+module Containing read_file function
+"""
+
+
+def read_file(filename=""):
+    """
+    reads a text file (UTF8) and prints it to stdout:
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
